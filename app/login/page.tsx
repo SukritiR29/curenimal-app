0ba4e845f06d81de06fb0fb870ea9ba36f8ecc42
SukritiR29 <sukritirajora@gmail.com>
2023-10-28 19:57:28 +0530

    logout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,21 @@
 
 import React from 'react'
 import { useSession, signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import router from 'next/dist/client/router';
+import Link from 'next/link';
 
 
 const Login = () => {
      const session = useSession();
         console.log(session);
+
+        const handleSignIn = async () => {
+          const result = await signIn('google', { callbackUrl: '/lostFound' });
+          if (result) {
+            router.push('/lostFound'); // Redirect to 'lostFound' after successful sign-in
+          }
+        };
 
     const backgroundImageUrl = 'url("/assests/curenimalbg.png")';
   return (
@@ -17,7 +27,12 @@ const Login = () => {
       <div className="max-w-md">
         <h1 className="mb-5 text-3xl text-white">Log Into Curenimal</h1>
         <div className='btn btn-accent text-neutral-focus mr-5 w-full'>
-        <button onClick={()=>signIn("google")}>Sign In with Google</button>
+        <button onClick={handleSignIn}>Sign In with Google</button>
+        </div>
+        <div className='mt-10'>
+          <Link href='/'>
+          <button className="btn btn-accent text-neutral-focus w-full">Home</button>
+          </Link>
         </div>
       </div>
       </div>
--- app/lostFound/page.tsx
+++ app/lostFound/page.tsx
@@ -1,9 +1,32 @@
+"use client";
 import React from 'react'
+import { useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const LostFound = () => {
+
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  }
+
+  if (!session) {
+    router.push('/login')
+    return null
+  }
+
+  const handleLogout = async () => {
+    await signOut(); // Sign out the user
+    router.push('/login'); // Redirect to the login page
+  };
+
+
   return (
     <div className='bg-bgColor h-screen'>
       <h1>Lost and Found</h1>
+      <button onClick={handleLogout} className="btn btn-accent text-neutral-focus ml-5">LogOut</button>
     </div>
   )
 }
